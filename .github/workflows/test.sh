3298d05a03980ff0dda0e4b13c05aba2f64b6595
goutham rajanala <gouthamsneham@gmail.com>
2023-07-18 22:11:39 +0530

    Update test.sh

--- .github/workflows/test.sh
+++ .github/workflows/test.sh
@@ -1,5 +1,11 @@
-curl -X POST \
-  -H "Authorization: Bearer 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" \
+  curl -X POST \
   -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/gouthamssc/Github_Workflow/actions/workflows/testwork.yaml/dispatches \
-  -d '{ "params": [ "param1", "param2" ] }'
+  -H "Authorization: Bearer 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" \
+  -H "Content-Type: application/json" \
+  -d '{
+    "ref": "main",
+    "inputs": {
+      "json_payload": "{ "params": [ "param1", "param2" ] }"
+    }
+  }' \
+  "https://api.github.com/repos/<YOUR_ORGANIZATION>/<YOUR_REPOSITORY>/actions/workflows/<YOUR_WORKFLOW_FILE>/dispatches"
